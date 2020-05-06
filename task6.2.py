f9e4d38a2ec45a4afb10bd5e3fa647b09844dacf
asdarov77 <asdarov77@mail.ru>
2020-05-06 19:20:42 +0300

    'task6.2'

--- task6.2.py
+++ task6.2.py
@@ -5,17 +5,18 @@ Created on Tue May  5 18:07:50 2020
 @author: Admin
 """
 
-i=0
+i=-1 # Номера строки начинаются с нуля
 birthday = input("Ведите дату рождения ddmmyy\n: ").strip()
 
 filename = 'pi_million_digits.txt'
 with open(filename) as file:   
     for line in file:
         line = line.strip()
+        i+=1 # Первая нулевая строка
         if birthday in line:
            print('счастливчик,в строке номер', i)
-        else:
-            i+=1
+        
+
         
 
 
